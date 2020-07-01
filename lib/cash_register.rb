e84823a1d6a966a8b63621dcfb1c2cc41996d605
Sara Amiry <saraamiry24@gmail.com>
2020-07-01 23:07:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
-
+class CashRegister
+  attr_accessor :total, :discount 
+  
+  def initialize(total = 0, discount = 20)
+    @total = total 
+    @discount = discount
+  end
+  
+  def add_item(title, price)
+    @total = title + price
+  end 
+end 
